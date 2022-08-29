69dfb83eca849085adecaae2f19d47c7294f36ca
Denison TS <106643117+denisonts@users.noreply.github.com>
2022-08-29 19:51:08 -0300

    Adicionar arquivos de projeto.

---
+++ VetorEmClasses/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace VetorEmClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] Lista = new Produto[n];
+
+            for (int i = 0; i < n; i++) {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Lista[i] = new Produto { NomeProduto = nome, Preco = preco };
+            }
+
+            double soma =0.0;
+            for (int i = 0; i < n; i++) {
+                soma += Lista[i].Preco;
+            }
+
+            double avg = soma / n;
+
+            Console.WriteLine("Media dos valores: " + avg);
+        }
+    }
+}
